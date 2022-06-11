cd2a2d893f6b6882f26c58f3b4b92c920796e954
Srijan Singh <srijanssj5goku@gmail.com>
2022-06-11 16:42:44 +0530

    Post Integration

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+// Middleware
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
+// Route
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -19,8 +22,9 @@ app.use(helmet());
 app.use(morgan("common"));
 
 //route
-app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend serever is running!")
